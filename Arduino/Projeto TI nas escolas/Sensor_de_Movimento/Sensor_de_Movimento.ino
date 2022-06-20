9a66ac353ac22c8c2e013c5d2e01f2d56d484340
Lucas Mateus <lmateus1067@outlook.com>
2022-06-20 11:18:01 -0300

    Melhorias no codigo

--- Arduino/Projeto TI nas escolas/Sensor_de_Movimento/Sensor_de_Movimento.ino
+++ Arduino/Projeto TI nas escolas/Sensor_de_Movimento/Sensor_de_Movimento.ino
@@ -1,38 +1,49 @@
-const int pinoSEN = 3; //PINO DIGITAL UTILIZADO PELO SENSOR DE PRESENÇA
-const int pinoLED1 = 4; //PINO DIGITAL UTILIZADO PELO LED 1
+const int pinoSEN = 3;  // PINO DIGITAL UTILIZADO PELO SENSOR DE PRESENÇA
+const int pinoLED1 = 4; // PINO DIGITAL UTILIZADO PELO LED 1
 const int pinoLED2 = 5; // PINO DIGITAL UTILIZADO PELO LED 2
 const int pinoLED3 = 6; // PINO DIGITAL UTILIZADO PELO LED 3
 const int pinoLED4 = 7; // PINO DIGITAL UTILIZADO PELO LED 4
-const int pinoBUZ = 8; // PINO DIGITAL UTILIZADO PELO BUZZER
+const int pinoBUZ = 8;  // PINO DIGITAL UTILIZADO PELO BUZZER
 
- 
-void setup(){
-  pinMode(pinoLED1, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  pinMode(pinoLED2, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  pinMode(pinoLED3, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  pinMode(pinoLED4, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  pinMode(pinoBUZ, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  pinMode(pinoSEN, INPUT); //DEFINE O PINO COMO ENTRADA
+void setup()
+{
+   pinMode(pinoLED1, OUTPUT); // DEFINE O PINO COMO SAÍDA
+   pinMode(pinoLED2, OUTPUT); // DEFINE O PINO COMO SAÍDA
+   pinMode(pinoLED3, OUTPUT); // DEFINE O PINO COMO SAÍDA
+   pinMode(pinoLED4, OUTPUT); // DEFINE O PINO COMO SAÍDA
+   pinMode(pinoBUZ, OUTPUT);  // DEFINE O PINO COMO SAÍDA
+   pinMode(pinoSEN, INPUT);   // DEFINE O PINO COMO ENTRADA
 }
-void loop(){
- if(digitalRead(pinoSEN) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
-    digitalWrite(pinoLED1, HIGH); //ACENDE O LED 1
-    digitalWrite(pinoLED2, HIGH); //ACENDE O LED 2
-    digitalWrite(pinoLED3, HIGH); //ACENDE O LED 3
-    digitalWrite(pinoLED4, HIGH); //ACENDE O LED 4
-    tone(pinoBUZ,1500);
-    delay(500);
-    digitalWrite(pinoLED1, LOW); //APAGA O LED 1
-    digitalWrite(pinoLED2, LOW); //APAGA O LED 2
-    digitalWrite(pinoLED3, LOW); //APAGA O LED 3
-    digitalWrite(pinoLED4, LOW); //APAGA O LED 4
-    delay(500);
-    noTone(pinoBUZ);
- }else{ //SENÃO, FAZ
-    digitalWrite(pinoLED1, LOW); //APAGA O LED 1
-    digitalWrite(pinoLED2, LOW); //APAGA O LED 2
-    digitalWrite(pinoLED3, LOW); //APAGA O LED 3
-    digitalWrite(pinoLED4, LOW); //APAGA O LED 4
-    noTone(pinoBUZ);
- }
+void loop()
+{
+
+   if (digitalRead(pinoSEN) == HIGH)
+   {                                // SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
+      digitalWrite(pinoLED1, HIGH); // ACENDE O LED 1
+      digitalWrite(pinoLED2, HIGH); // ACENDE O LED 2
+      digitalWrite(pinoLED3, HIGH); // ACENDE O LED 3
+      digitalWrite(pinoLED4, HIGH); // ACENDE O LED 4
+      digitalWrite(pinoBUZ, HIGH);  // LIGA O BUZZER
+      delay(500);
+      digitalWrite(pinoLED1, LOW); // APAGA O LED 1
+      digitalWrite(pinoLED2, LOW); // APAGA O LED 2
+      digitalWrite(pinoLED3, LOW); // APAGA O LED 3
+      digitalWrite(pinoLED4, LOW); // APAGA O LED 4
+      digitalWrite(pinoBUZ, LOW),  // DESLIGA O BUZZZER
+          delay(200);
+      digitalWrite(pinoLED1, HIGH); // ACENDE O LED 1
+      digitalWrite(pinoLED2, HIGH); // ACENDE O LED 2
+      digitalWrite(pinoLED3, HIGH); // ACENDE O LED 3
+      digitalWrite(pinoLED4, HIGH); // ACENDE O LED 4
+      digitalWrite(pinoBUZ, HIGH);  // LIGA O BUZZER
+   }
+
+   else
+   {                               // SENÃO, FAZ
+      digitalWrite(pinoLED1, LOW); // APAGA O LED 1
+      digitalWrite(pinoLED2, LOW); // APAGA O LED 2
+      digitalWrite(pinoLED3, LOW); // APAGA O LED 3
+      digitalWrite(pinoLED4, LOW); // APAGA O LED 4
+      digitalWrite(pinoBUZ, LOW);  // DESLIGA O BUZZER
+   }
 }
