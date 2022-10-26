6d2feac5cfb6d11b9da6ce31e769b884fe102eb3
Lucas Mateus <lmateus1067@outlook.com>
2022-10-26 09:40:46 -0300

    add code

--- Arduino/Projetos Pessoais/teste_sensor/teste_sensor.ino
+++ Arduino/Projetos Pessoais/teste_sensor/teste_sensor.ino
@@ -2,95 +2,82 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <DHT.h>
+#include <ThingSpeak.h>
 
-// Uncomment one of the lines below for whatever DHT sensor type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTPIN 4
+#define DHTTYPE DHT22
 
-/*Put your SSID & Password*/
-const char* ssid = "UFRN_CERES";  // Enter SSID here
-const char* password = "UFRN@1306";  //Enter Password here
+WiFiClient client;
+DHT dht(DHTPIN, DHTTYPE); // Sensor that reads the temperature and humidity
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "IJR99VIKPE8KS1Y8";
 
-ESP8266WebServer server(80);
 
-// DHT Sensor
-uint8_t DHTPin = 4; 
-               
-// Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);                
+//conect wifi
+const char* ssid = "UFRN_CERES"; //Enter your WIFI ssid
+const char* password = "UFRN@1306"; //Enter your WIFI password
 
-float Temperature;
-float Humidity;
- 
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+
+// Variable to hold temperature readings
+float temperatureC;
+float humidity;
+
+
+
 void setup() {
-  Serial.begin(115200);
-  delay(100);
+  dht.begin();
+  Serial.begin(115200);  //Initialize serial
+  WiFi.mode(WIFI_STA);   
   
-  pinMode(DHTPin, INPUT);
-
-  dht.begin();              
-
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  //connect to your local wi-fi network
-  WiFi.begin(ssid, password);
-
-  //check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
-
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
-
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
+
 void loop() {
-  server.handleClient();
-  
-}
+  if ((millis() - lastTime) > timerDelay) {
+    
+    // Connect or reconnect to WiFi
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.print("Attempting to connect");
+      while(WiFi.status() != WL_CONNECTED){
+        WiFi.begin(ssid, password); 
+        delay(5000);     
+      } 
+      Serial.println("\nConnected.");
+    }
 
-void handle_OnConnect() {
+    // Get a new temperature reading
+    temperatureC = dht.readTemperature();
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureC);
+    humidity = dht.readHumidity();
+    Serial.print("Humidity (%): ");
+    Serial.println(humidity);
+    
+    //uncomment if you want to get temperature in Fahrenheit
+    /*temperatureF = 1.8 * bme.readTemperature() + 32;
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureF);*/
 
- Temperature = dht.readTemperature(); // Gets the values of the temperature
-  Humidity = dht.readHumidity(); // Gets the values of the humidity 
-  server.send(200, "text/html", SendHTML(Temperature,Humidity)); 
-}
+    // set the fields with the values
+    ThingSpeak.setField(1, temperatureC);
+    ThingSpeak.setField(2, humidity);
+    
+    
+    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+    // pieces of information in a channel.  Here, we write to field 1.
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    //uncomment if you want to get temperature in Fahrenheit
+    //int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureF, myWriteAPIKey);
 
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float Temperaturestat,float Humiditystat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP8266 Weather Report</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<div id=\"webpage\">\n";
-  ptr +="<h1>ESP8266 NodeMCU Weather Report</h1>\n";
-  
-  ptr +="<p>Temperature: ";
-  ptr +=(int)Temperaturestat;
-  ptr +="°C</p>";
-  ptr +="<p>Humidity: ";
-  ptr +=(int)Humiditystat;
-  ptr +="%</p>";
-  
-  ptr +="</div>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
+    if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    lastTime = millis();
+  }
 }
